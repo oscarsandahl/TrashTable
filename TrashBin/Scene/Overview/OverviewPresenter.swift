b492fe59387ed00fb85aafcff33c6455548caf27
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 18:42:47 +0100

    fetch all documents to presenter

--- TrashBin/Scene/Overview/OverviewPresenter.swift
+++ TrashBin/Scene/Overview/OverviewPresenter.swift
@@ -9,8 +9,36 @@ import Foundation
 
 class OverviewPresenter {
     weak var view: ViewController?
+    var documents: [DocumentsViewModel] = []
     
     init(_ view: ViewController) {
         self.view = view
+    }
+    
+    func fetchAllDocumentsFromBackend() {
+        fetchDocumentsFromBackend()
+        fetchReceiptFromBackend()
+    }
+    
+    func fetchDocumentsFromBackend() {
+        BackendService.shared.loadDocuments { result in
+            switch result {
+            case .success(let result):
+                self.documents.append(contentsOf: result)
+            default:
+                print("Show error alert here")
+            }
+        }
+    }
+    
+    func fetchReceiptFromBackend() {
+        BackendService.shared.loadReceipt { result in
+            switch result {
+            case .success(let result):
+                self.documents.append(contentsOf: result)
+            default:
+                print("Show error alert here")
+            }
+        }
     }
 }
