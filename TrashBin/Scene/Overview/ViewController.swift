fe3c825a1c2ef27afa14f5449afc47e7d3d8fee8
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 20:28:28 +0100

    add title to viewcontroller

--- TrashBin/Scene/Overview/ViewController.swift
+++ TrashBin/Scene/Overview/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.title = "Papperskorg"
         setupTableView()
         presenter.fetchAllDocumentsFromBackend()
     }
