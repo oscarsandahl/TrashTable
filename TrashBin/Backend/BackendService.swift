c1d532779add925c251bea8bbf9e1529274eab6d
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 17:48:26 +0100

    add function to load receipts

--- TrashBin/Backend/BackendService.swift
+++ TrashBin/Backend/BackendService.swift
@@ -27,4 +27,22 @@ class BackendService {
             }
         }
     }
+    
+    func loadReceipt(callback: @escaping BackendCallback<[DocumentsViewModel]>) {
+        var documents: [DocumentsViewModel] = []
+        
+        if let location = Bundle.main.url(forResource: "Receipts", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: location)
+                let jsonDecoder = JSONDecoder()
+                let dataFromJson = try jsonDecoder.decode(ReceiptRoot.self, from: data)
+                dataFromJson.receipts.forEach { item in
+                    documents.append(DocumentsViewModel(title: item.store_name, date: item.purchase_date, sender: nil, receiptType: item.type, fileType: .receipt))
+                }
+                callback(.success(documents))
+            } catch  {
+                callback(.failure(.empty))
+            }
+        }
+    }
 }
