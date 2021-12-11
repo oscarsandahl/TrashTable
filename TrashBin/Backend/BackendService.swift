d06795ef49f420adb6cf86805226dcab4543c55e
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 17:39:00 +0100

    add function to load documents from api

--- TrashBin/Backend/BackendService.swift
+++ TrashBin/Backend/BackendService.swift
@@ -9,4 +9,22 @@ import Foundation
 
 class BackendService {
     static let shared = BackendService()
+    
+    func loadDocuments(callback: @escaping BackendCallback<[DocumentsViewModel]>) {
+        var documents: [DocumentsViewModel] = []
+        
+        if let location = Bundle.main.url(forResource: "Documents", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: location)
+                let jsonDecoder = JSONDecoder()
+                let dataFromJson = try jsonDecoder.decode([DocumentModel].self, from: data)
+                dataFromJson.forEach { item in
+                    documents.append(DocumentsViewModel(title: item.subject, date: item.generated_at, sender: item.sender_name, receiptType: nil, fileType: .document))
+                }
+                callback(.success(documents))
+            } catch  {
+                callback(.failure(.empty))
+            }
+        }
+    }
 }
