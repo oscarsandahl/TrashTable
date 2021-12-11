e26a96ecb4f2e766530d969ed7a8c0228e6f998d
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 18:02:25 +0100

    add cells to receipt and document

---
+++ TrashBin/Scene/Overview/Cell/DocumentCell.swift
@@ -0,0 +1,24 @@
+//
+//  DocumentCell.swift
+//  TrashBin
+//
+//  Created by Sandahl, Oscar on 2021-12-11.
+//
+
+import UIKit
+
+class DocumentCell: UITableViewCell {
+    static let cellIdentifier = "documentCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ TrashBin/Scene/Overview/Cell/ReceiptCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReceiptCell.swift
+//  TrashBin
+//
+//  Created by Sandahl, Oscar on 2021-12-11.
+//
+
+import UIKit
+
+class ReceiptCell: UITableViewCell {
+    static let cellIdentifier = "receiptCell"
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
