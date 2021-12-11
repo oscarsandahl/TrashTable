3c61f5c4bfce2668dd4ddfeed709b4491016c5e2
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 18:10:47 +0100

    add tableview and setup

--- TrashBin/Scene/Overview/ViewController.swift
+++ TrashBin/Scene/Overview/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     var presenter: OverviewPresenter!
+    let tableView: UITableView = UITableView()
     
     class func initViewController() -> UIViewController {
         let viewController = ViewController()
@@ -18,8 +19,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupTableView()
     }
 
+    func setupTableView() {
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        tableView.register(DocumentCell.self, forCellReuseIdentifier: DocumentCell.cellIdentifier)
+        tableView.register(ReceiptCell.self, forCellReuseIdentifier: ReceiptCell.cellIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+}
