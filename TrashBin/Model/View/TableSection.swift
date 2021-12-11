06432d70b81c553ce3a10352852d401545eb2898
Oscar <oscarsandahl@deloitte.co.uk>
2021-12-11 21:21:01 +0100

    add view model for sections

---
+++ TrashBin/Model/View/TableSection.swift
@@ -0,0 +1,13 @@
+//
+//  TableSection.swift
+//  TrashBin
+//
+//  Created by Sandahl, Oscar on 2021-12-11.
+//
+
+import Foundation
+
+struct TableSection {
+    var month: Int
+    var documents: [DocumentsViewModel]
+}
